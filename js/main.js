862ce8dc8d9e9243aa1cdc8cd2ff262694fd953c
abutolib <abutolib12042002@gmail.com>
2022-11-08 22:21:51 +0500

    the end

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var elOption1 = document.querySelector(".option1");
+var elOption1Btn = document.querySelector(".option1__btn")
+
+elOption1Btn.addEventListener("click",function(){
+  elOption1.classList.toggle("open")
+})
